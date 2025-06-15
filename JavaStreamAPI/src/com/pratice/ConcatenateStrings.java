e94cf4eb9d7fcfa8768f2bbb61236474ed717840
Papun <papun.1424@gmail.com>
2025-06-15 09:18:54 +0530

    ConcatenateStrings

---
+++ JavaStreamAPI/src/com/pratice/ConcatenateStrings.java
@@ -0,0 +1,15 @@
+package com.pratice;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ConcatenateStrings {
+
+	public static void main(String[] args) {
+		List<String> names = Arrays.asList("Papun", "Kumar", "Mohapatra");
+		String ops = names.stream().collect(Collectors.joining(" "));
+		System.out.println(ops);
+	}
+
+}
